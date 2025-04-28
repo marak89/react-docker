9a6677c146153011ca09af5f6b7281b7619a347f
Marcin Rał <marcin@m89.eu>
2025-04-28 10:11:13 +0200

    fix

--- app/src/App.js
+++ app/src/App.js
@@ -11,7 +11,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
 
-          <Welcome name="Sara" phone="12345" />
+          <Welcome name={"Sara"} phone="12345" />
 		  <Counter />
           <TytulDynamiczny />
           <UserList />
